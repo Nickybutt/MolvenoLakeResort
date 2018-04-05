142b5238f1bdc0d6076a33e138358ffc94845f79
AceCoderYoda <workingmanforums@outlook.com>
2018-04-05 11:14:04 +0200

    Added basic user interface

---
+++ MolvenoLakeResort.Library.Authentication/User.cs
@@ -0,0 +1,63 @@
+﻿#region ...   [Header]   ...
+
+// Solution      ::    MolvenoLakeResort
+// Filename      ::    MolvenoLakeResort.Library.Authentication.User.cs
+// Created On    ::    05/04/2018 9:23 AM
+// Altered On    ::    05/04/2018 9:28 AM
+// By            ::    Arjan Crielaard
+
+#endregion
+
+#region ...   [Usings]   ...
+
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+#endregion
+
+namespace MolvenoLakeResort.Library.Authentication
+{
+    public class User
+    {
+
+        public User()
+        {
+            Id = Guid.NewGuid();
+            UserLevel = 100;
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                var valid = !string.IsNullOrEmpty(Password) && !string.IsNullOrEmpty(Email);
+                if (valid)
+                {
+                    valid = Password.Equals(ConfirmPassword);
+                }
+
+                return valid;
+            }
+        }
+
+        public Guid Id { get; }
+
+        [DisplayName("Username")]
+        [Required(ErrorMessage = "The email address is required.")]
+        [EmailAddress(ErrorMessage = "Invalid e-mail address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required.")]
+        [Compare("Password", ErrorMessage = "Passwords do not match. Please try again.")]
+        public string ConfirmPassword { get; set; }
+        
+        public int UserLevel { get; set; }
+
+        public bool? RememberMe { get; set; }
+    }
+}
---
+++ MolvenoLakeResort/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿#region ...   [Header]   ...
+
+// Solution      ::    MolvenoLakeResort
+// Filename      ::    MolvenoLakeResort.UserController.cs
+// Created On    ::    05/04/2018 9:59 AM
+// Altered On    ::    05/04/2018 10:28 AM
+// By            ::    Arjan Crielaard
+
+#endregion
+
+#region ...   [Usings]   ...
+
+using System.Web.Mvc;
+using MolvenoLakeResort.Library.Authentication;
+
+#endregion
+
+namespace MolvenoLakeResort.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Register
+        public ActionResult Register()
+        {
+            var model = new User();
+            return View(model);
+        }
+
+        // POST: Register
+        [HttpPost]
+        public ActionResult Register(User model)
+        {
+            return model.IsValid ? View("Index") : View();
+        }
+
+        public void Login()
+        {
+        }
+
+        public void Logout()
+        {
+        }
+    }
+}
